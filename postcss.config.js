e56ede44b712404c46e0852c323dca81ed1f2d1d
Brian Sipple <brsipple42@gmail.com>
2017-05-26 03:32:56 -0700

    remove postcss-cssnext from transformations (import only)

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,10 @@
 /* eslint-env node */
 
 module.exports = function (ctx) {
-  const { file: { basename: CURRENT_FILE_BASE_NAME } } = ctx;
-
   return {
     plugins: {
       // ⚠️ Order matters! PostCSS will run plugins in the order listed.
       'postcss-import': {},
-
-      'postcss-cssnext': {
-        features: {
-          customProperties: {
-            preserve: CURRENT_FILE_BASE_NAME !== 'index.css'
-          }
-        }
-      },
 
       // 🔑 Set `process.env.NODE_ENV` to "production" to create
       // a minified production build
